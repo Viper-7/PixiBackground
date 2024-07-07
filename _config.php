f44f1de0e651c5bc14dd25e747090f35816e67b9
Dale Horton <viper7@viper-7.com>
2024-07-07 23:18:29 +1000

    Fixed crashes on module import

--- _config.php
+++ _config.php
@@ -2,21 +2,22 @@
 use SilverStripe\View\Requirements;
 use SilverStripe\SiteConfig\SiteConfig;
 
+if(SiteConfig::current_site_config()->hasField('CharacterSpeed')) {
+    $vars = [
+        'InitialDirt' => SiteConfig::current_site_config()->InitialDirt,
+        'DirtyRate' => SiteConfig::current_site_config()->DirtyRate,
+        'CharacterSize' => SiteConfig::current_site_config()->CharacterSize,
+        'CharacterSpeed' => SiteConfig::current_site_config()->CharacterSpeed,
+    ];
+}
 
-$vars = [
-    'InitialDirt' => SiteConfig::current_site_config()->InitialDirt,
-    'DirtyRate' => SiteConfig::current_site_config()->DirtyRate,
-    'CharacterSize' => SiteConfig::current_site_config()->CharacterSize,
-    'CharacterSpeed' => SiteConfig::current_site_config()->CharacterSpeed,
-];
-
-if(in_array('SpriteID', array_keys(SiteConfig::current_site_config()))) {
+if(SiteConfig::current_site_config()->hasField('SpriteID')) {
     $vars['Sprite'] = SiteConfig::current_site_config()->Sprite()->getAbsoluteURL();
 } else {
     $vars['Sprite'] = null;
 }
 
-if(!SiteConfig::current_site_config()->DisableBackground) {
+if(SiteConfig::current_site_config()->hasField('DisableBackground') && !SiteConfig::current_site_config()->DisableBackground) {
     Requirements::javascript('viper-7/pixibackground:dist/js/pixi.min.js');
     Requirements::javascriptTemplate('viper-7/pixibackground:dist/js/pixi_background.js', $vars);
 }
