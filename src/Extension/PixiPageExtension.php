89dd40577993eaee014014a4e54123023d45b693
Viper-7 <viper7@viper-7.com>
2024-07-08 01:33:47 +1000

    Update PixiPageExtension.php

--- src/Extension/PixiPageExtension.php
+++ src/Extension/PixiPageExtension.php
@@ -7,26 +7,9 @@ use SilverStripe\SiteConfig\SiteConfig;
 
 class PixiPageExtension extends Extension {
     public function onAfterInit() {
-        if(SiteConfig::current_site_config()->hasField('CharacterSpeed')) {
-            $vars = [
-                'InitialDirt' => SiteConfig::current_site_config()->InitialDirt,
-                'DirtyRate' => SiteConfig::current_site_config()->DirtyRate,
-                'MaxDirtSize' => SiteConfig::current_site_config()->MaxDirtSize,
-                'FlipDirections' => SiteConfig::current_site_config()->FlipDirections ? 'true' : 'false',
-                'CharacterSize' => SiteConfig::current_site_config()->CharacterSize,
-                'CharacterSpeed' => SiteConfig::current_site_config()->CharacterSpeed,
-            ];
-        }
-        
-        if(SiteConfig::current_site_config()->hasField('SpriteID')) {
-            $vars['Sprite'] = SiteConfig::current_site_config()->Sprite()->getURL();
-        } else {
-            $vars['Sprite'] = null;
-        }
-        
-        if(SiteConfig::current_site_config()->hasField('DisableBackground') && !SiteConfig::current_site_config()->DisableBackground) {
+        if(!SiteConfig::current_site_config()->hasField('DisableBackground') || !SiteConfig::current_site_config()->DisableBackground) {
             Requirements::javascript('viper-7/silverstripe-animation-:dist/js/pixi.min.js');
-            Requirements::javascriptTemplate('viper-7/silverstripe-animation-:dist/js/pixi_background.js', $vars);
+            Requirements::javascriptTemplate('viper-7/silverstripe-animation-:dist/js/pixi_background.js', ['SiteConfig' => SiteConfig::current_site_config()]);
         }
     }
 }
